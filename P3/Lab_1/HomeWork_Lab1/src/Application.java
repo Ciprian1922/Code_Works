ffa5f5f2f7873e5c9e9c850874eba99a851d1ff5
Popa Ioan-Ciprian <popaciprian2003@gmail.com>
2023-10-09 21:27:47 +0300

    Homework for lab one on p3 was completed

--- P3/Lab_1/HomeWork_Lab1/src/Application.java
+++ P3/Lab_1/HomeWork_Lab1/src/Application.java
@@ -1,15 +1,42 @@
-public class Application{
+public class Application {
     private InputDevice inputDevice;
     private OutputDevice outputDevice;
-    public Application(InputDevice inp, OutputDevice out){
+
+    public Application(InputDevice inp, OutputDevice out) {
         this.inputDevice = inp;
         this.outputDevice = out;
     }
-    public void run(){
+
+    public void run() {
         outputDevice.writeMessage("Application started");
         outputDevice.writeMessage("Today’s lucky numbers are: ");
-        for(int i = 0 ; i < 2 ; i++) {
-            outputDevice.writeMessage(inputDevice.nextInt());
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 < 5 && p2 < 5) {
+            int H = inputDevice.nextInt();
+            int S = inputDevice.nextInt();
+
+            if (H == S) {
+                p1 += 2;
+                p2 += 2;
+            } else if (S >= 10 && H % S == 0) {
+                p2++;
+            } else if (H >= 10 && S % H == 0) {
+                p1++;
+            }
+
+            outputDevice.writeMessage("***********************");
+            outputDevice.writeMessage("Player H:" + p1 + ", He picked: " + H);
+            outputDevice.writeMessage("Player S:" + p2 + ", He picked: " + S);
+        }
+
+        if (p1 == p2) {
+            outputDevice.writeMessage("Tie");
+        } else if (p1 > p2) {
+            outputDevice.writeMessage("Player H won!");
+        } else {
+            outputDevice.writeMessage("Player S won!");
         }
     }
 }
--- P3/Lab_1/HomeWork_Lab1/src/OutputDevice.java
+++ P3/Lab_1/HomeWork_Lab1/src/OutputDevice.java
@@ -1,6 +1,6 @@
 public class OutputDevice {
     public void writeMessage(String mess){
-        System.out.println(mess+"he");
+        System.out.println(mess);
     }
     public void writeMessage(int mess){
         String number = Integer.toString(mess);
