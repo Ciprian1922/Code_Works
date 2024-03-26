963dc8b8446e2c270b29775c83b00aa62c41339a
Popa Ioan-Ciprian <popaciprian2003@gmail.com>
2024-03-26 15:18:33 +0200

    2fragments

--- Y2S2/PMD/Fragments/app/src/main/java/com/example/fragments/MainActivity.java
+++ Y2S2/PMD/Fragments/app/src/main/java/com/example/fragments/MainActivity.java
@@ -14,11 +14,33 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void selectFactorialFragment(View view) {
+    public void selectFirstFactorialFragment(View view) {
+        // Create a new instance of FactorialFragment
         FactorialFragment factorialFragment = new FactorialFragment();
+
+        // Get the FragmentManager and start a FragmentTransaction
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the existing fragment (if any) with the FactorialFragment
         fragmentTransaction.replace(R.id.fragment_place, factorialFragment);
+
+        // Commit the transaction
+        fragmentTransaction.commit();
+    }
+
+    public void selectSecondFactorialFragment(View view) {
+        // Create a new instance of FactorialFragment
+        FactorialFragment factorialFragment = new FactorialFragment();
+
+        // Get the FragmentManager and start a FragmentTransaction
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the existing fragment (if any) with the FactorialFragment
+        fragmentTransaction.replace(R.id.fragment_place_2, factorialFragment);
+
+        // Commit the transaction
         fragmentTransaction.commit();
     }
 }
